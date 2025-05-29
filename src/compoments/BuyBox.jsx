a818709d1f8d338944be423e6f2bc8b0500da2bc
Leo-Gomes <devleogomes@gmail.com>
2025-05-28 23:16:37 -0300

    buybox, carousel

--- src/compoments/BuyBox.jsx
+++ src/compoments/BuyBox.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ProductOptions } from "./ProductOptions";
+import { RatingDisplay } from "./RatingDisplay";
 
 export const BuyBox = ({ name, reference, stars, rating, priceDiscount, price, description }) => {
     const { id } = useParams();
@@ -14,17 +15,19 @@ export const BuyBox = ({ name, reference, stars, rating, priceDiscount, price, d
     return (
         <div className="ml-10 flex flex-col justify-between">
             <h2 className="font-bold text-[32px] h-18">{productView.titulo}</h2>
-            <p className="text-xs text-dark-gray-3">Casual | {productView.marca} | {productView.reference}</p>
-            <div>
-                <span>{productView.stars}</span> <span>{productView.rating}</span>
+            <p className="text-xs text-dark-gray-3 mb-2">Casual | {productView.marca} | {productView.reference}</p>
+
+            <div className="flex">
+            <RatingDisplay rating={productView.stars || 0} />
+                <span className="text-[#8f8f8f]">({productView.rating} avaliações)</span>
             </div>
 
-            <div>
-                <p>{productView.priceDiscount}</p> <p>{price}</p>
+            <div className="my-4">
+            {productView.priceDiscount ? <> <span  className="mt-2 ">R$ <span className="font-bold text-[32px]">{productView.priceDiscount}</span>,00</span><span className="mt-2 line-through text-light-gray-2 font-extralight ml-2">{productView.preco},00</span></> : <p className="mt-2 font-bold">${productView.preco},00</p>}
             </div>
 
             <h2 className="font-bold text-sm text-light-gray">Descrição do produto</h2>
-            <p className="text-sm text-dark-gray-2">{productView.description}</p>
+            <p className="text-sm text-dark-gray-2 w-[75%]">{productView.description}</p>
 
             <h2 className="font-bold text-sm text-light-gray ">Tamanho</h2>
 
--- src/compoments/Gallery .jsx
+++ src/compoments/Gallery .jsx
@@ -1,27 +1,30 @@
 import { Carousel } from 'antd';
 import '../index.css'
-import { BuyBox } from './BuyBox';
-const contentStyle = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const produtos = [1, 2, 3, 4, 5]
-const Gallery = () => (
+
+const Gallery = () => {
+  const {id} = useParams();
+  const [productView, setProductView] = useState({});
+  useEffect(() => {
+      axios.get(`https://api-products-kpiv.onrender.com/produtos/${id}`)
+          .then(data => setProductView(data.data))
+          .catch(err => console.error("Erro ao buscar produtos: ", err));
+  }, [])
+  return (
   <div className='w-[49%]'>
     
     <Carousel arrows infinite={false} dots={false}>
       
       {produtos.map((item, index) => (
-        <div key={index} className='w-full bg-blue-500 text-white text-center h-100 content-center'>item {item}</div>
+        <div key={index} className={`w-full bg-[#ccc] text-white h-120 content-center`}><img src={`${productView.img}`}alt="" className='mx-auto w-120'/>{console.log(item)}</div>
       ))}
       
     </Carousel>
   </div>
-  
-);
+  )}
+;
 export default Gallery;
